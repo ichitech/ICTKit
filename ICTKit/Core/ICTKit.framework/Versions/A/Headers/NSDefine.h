2a737e3dd5f3ff7c54430a68cf3ffc1b37b43b12
Ngocbinh02 <ngocbinh02@gmail.com>
2015-06-24 13:35:28 +0700

    added

---
+++ ICTKit/Core/ICTKit.framework/Versions/A/Headers/NSDefine.h
@@ -0,0 +1,47 @@
+/*!
+ @header    NSDefine.h
+ @abstract  ICTKit iOS SDK Source
+ @copyright Copyright 2013 IchiTech. All rights reserved.
+ @version  6.0
+ */
+
+#if __has_feature(objc_arc)
+#define stabilized strong
+#define NSDEALOC(object)
+#else
+#define stabilized retain
+#define NSDEALOC(object)  [object dealoc]
+#endif
+
+/*!
+ * @abstract Define NSAsser
+ */
+#define CALL_CASH_APP(msg) [[NSAssertionHandler currentHandler] handleFailureInMethod:_cmd object:self file:[NSString stringWithUTF8String:__FILE__] lineNumber:__LINE__ description:msg]
+
+
+/*!
+ * @struct NSLength.
+ */
+typedef struct _NSLength {
+    NSUInteger minimum;
+    NSUInteger maximum;
+} NSLength;
+
+NS_INLINE NSLength NSMakeLength(NSUInteger minimum, NSUInteger maximum) {
+    NSLength r;
+    r.minimum = minimum;
+    r.maximum = maximum;
+    return r;
+}
+
+
+#pragma mark - Define
+
+#define USER_DEFAULTS [NSUserDefaults standardUserDefaults]
+#define LOAD_NIB(nibName) [[[NSBundle mainBundle] loadNibNamed:nibName owner:self options:nil] objectAtIndex:0]
+
+
+
+
+
+
---
+++ ICTKit/Core/ICTKit.framework/Versions/A/Headers/UIPickerSheet.h
@@ -0,0 +1,27 @@
+/*!
+ @header    UIPickerSheet.h
+ @abstract  ICTKit iOS SDK Source
+ @copyright Copyright 2013 Global CyberSoft. All rights reserved.
+ @version  6.0
+ */
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@class UIPickerSheet;
+
+typedef void(^UIPickerSheetCompletion)(UIPickerSheet *picker,BOOL finish);
+
+@interface UIPickerSheet : NSObject
+
+@property (nonatomic, strong, readonly) UINavigationBar     *navigationBar;
+@property (nonatomic, strong, readonly) UIPickerView        *pickerView;
+@property (nonatomic, strong, readonly) UIView              *backgroundView;
+@property (nonatomic, strong, readonly) UIBarButtonItem     *leftBarButtonItem;
+@property (nonatomic, strong, readonly) UIBarButtonItem     *rightBarButtonItem;
+@property (nonatomic, strong) UIPickerSheetCompletion       finishBlock;
+@property (nonatomic, assign) NSTimeInterval                timeAnimation;
+
+- (void)showCompletion:(UIPickerSheetCompletion)completion;
+
+@end
