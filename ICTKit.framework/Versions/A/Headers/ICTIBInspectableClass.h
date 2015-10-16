d2530765cfb2c9ae4577459ec60635f80eb6cf90
Harry-MacMini <ngocbinh02@gmail.com>
2015-10-17 00:05:35 +0700

    updated

--- ICTKit.framework/Versions/A/Headers/ICTIBInspectableClass.h
+++ ICTKit.framework/Versions/A/Headers/ICTIBInspectableClass.h
@@ -8,6 +8,8 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+IB_DESIGNABLE
+
 @interface UIImageView (IBInspectableClass)
 
 @property (nonatomic, strong) IBInspectable UIColor *imageTintColor ;
--- ICTKit.framework/Versions/A/Headers/UIViewController+Helper.h
+++ ICTKit.framework/Versions/A/Headers/UIViewController+Helper.h
@@ -10,24 +10,24 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef void (^UIViewControllerSegueBlock) (UIStoryboardSegue *segue, id _Nullable sender);
+typedef void (^UIViewControllerSegueBlock) (UIStoryboardSegue *segue, id _Nullable sender)  NS_AVAILABLE_IOS(6_0);
 
 @interface UIViewController (BlockSegue)
 
-- (void)configureSegue:(NSString *)identifier withBlock:(UIViewControllerSegueBlock _Nullable)block;
-- (void)performSegueWithIdentifier:(NSString *)identifier sender:(id _Nullable)sender withBlock:(UIViewControllerSegueBlock _Nullable)block;
+- (void)configureSegue:(NSString *)identifier withBlock:(UIViewControllerSegueBlock _Nullable)block NS_AVAILABLE_IOS(6_0);
+- (void)performSegueWithIdentifier:(NSString *)identifier sender:(id _Nullable)sender withBlock:(UIViewControllerSegueBlock _Nullable)block NS_AVAILABLE_IOS(6_0);
 
 @end
 
 @interface UIViewController (Visible)
 
-- (BOOL)isVisible;
+- (BOOL)isVisible NS_AVAILABLE_IOS(6_0);
 
 @end
 
 @interface UIViewController (Modal)
 
-- (BOOL) isModal;
+- (BOOL) isModal NS_AVAILABLE_IOS(4_0);
 
 @end
 
