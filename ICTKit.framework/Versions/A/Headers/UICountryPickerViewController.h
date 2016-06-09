aac8bcf65d1e4dfb9e5d290c5ed9c4865cc4c964
Ngocbinh02 <ngocbinh02@gmail.com>
2016-06-09 14:10:32 +0700

    updated country picker

--- ICTKit.framework/Versions/A/Headers/UICountryPickerViewController.h
+++ ICTKit.framework/Versions/A/Headers/UICountryPickerViewController.h
@@ -14,15 +14,15 @@ NS_ASSUME_NONNULL_BEGIN
 //setting
 typedef void(^CountryPickerViewControllerComposer)(UICountryPickerViewController *picker);
 //compose
-typedef void(^CountryPickerViewControllerCompletion)(BOOL success, UICountryPickerViewController *picker, NSCountryItem *info);
+typedef void(^CountryPickerViewControllerCompletion)(BOOL success,  UICountryPickerViewController * _Nonnull picker, NSCountryItem * _Nullable info);
 
 @protocol UICountryPickerViewControllerDelegate<NSObject>
 @optional
 // The picker does not dismiss itself; the client dismisses it in these callbacks.
 // The delegate will receive one or the other, but not both, depending whether the user
 // confirms or cancels.
-- (void)countryPickerController:(UICountryPickerViewController *)picker didFinishPickingCountryWithInfo:(NSCountryItem *)info;
-- (void)countryPickerControllerDidCancel:(UICountryPickerViewController *)picker;
+- (void)countryPickerController:(UICountryPickerViewController *)picker didFinishPickingCountryWithInfo:(NSCountryItem *)info NS_UNAVAILABLE;
+- (void)countryPickerControllerDidCancel:(UICountryPickerViewController *)picker NS_UNAVAILABLE;
 
 @end
 
@@ -37,9 +37,10 @@ typedef void(^CountryPickerViewControllerCompletion)(BOOL success, UICountryPick
 - (instancetype) initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
 - (instancetype) initWithNibName:(nullable NSString *)nibNameOrNil bundle:(nullable NSBundle *)nibBundleOrNil NS_UNAVAILABLE;
 
-@property (nonatomic, strong, readonly)         NSArray *countries;
+@property (nonatomic, strong, readonly)         NSArray<NSDictionary *> *countries;
 @property (nonatomic, getter=isPhoneCodeHidden) BOOL phoneCodeHidden; // Defaults to NO, i.e. no hidden.
 @property (nonatomic, strong, readonly)         UIBarButtonItem *leftBarButtonItem;
+@property (nonatomic, strong, readonly)         UIViewController *viewController;
 
 - (void) setBarTitle:(nullable NSString *)title;
 
@@ -52,12 +53,19 @@ typedef void(^CountryPickerViewControllerCompletion)(BOOL success, UICountryPick
 @interface NSCountryItem : NSObject
 
 - (instancetype) init NS_UNAVAILABLE;
+- (instancetype) initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
 - (instancetype) initWithISOcountryCode:(NSString *)ISOcountryCode;
+
++ (NSArray <NSDictionary*>*) allCountries;
++ (nullable NSCountryItem *) countryItemForISOcountryCode:(NSString *)ISOcountryCode;
++ (nullable NSCountryItem *) countryItemForCountry:(NSString *)country;
++ (nullable NSCountryItem *) countryItemForPhoneCode:(NSString *)phoneCode;
 
 @property (nonatomic, strong, readonly) NSString    *ISOcountryCode;
 @property (nonatomic, strong, readonly) NSString    *country;
 @property (nonatomic, strong, readonly) NSString    *phoneCode;
 @property (nonatomic, strong, readonly) UIImage     *flagImage;
+@property (nonatomic, strong, readonly) NSString    *flagPath;
 @property (nonatomic, strong, readonly) NSDictionary*userInfo;
 
 @end
